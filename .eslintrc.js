955af85456007ee92a05a499324dd57d47b77ac0
Rafał Majewski <goodheropl@gmail.com>
2022-11-03 23:58:43 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+	root: true,
+	overrides: [
+		{
+			files: ["*.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "./tsconfig.json",
+				tsconfigRootDir: __dirname,
+				sourceType: "module",
+				ecmaVersion: 13,
+			},
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+			plugins: ["@typescript-eslint"],
+			rules: {
+				"no-empty-function": "off",
+				"@typescript-eslint/no-empty-function": "off",
+			},
+		},
+		{
+			files: ["*.test.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "./tsconfig.json",
+				tsconfigRootDir: __dirname,
+				sourceType: "module",
+				ecmaVersion: 13,
+			},
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+			plugins: ["@typescript-eslint"],
+			rules: {
+				"no-empty-function": "off",
+				"@typescript-eslint/no-empty-function": "off",
+			},
+		},
+		{
+			files: ["*.js"],
+			parser: "espree",
+			parserOptions: {
+				ecmaVersion: 13,
+				sourceType: "module",
+			},
+			extends: ["eslint:recommended", "prettier"],
+			rules: {
+				"no-empty-function": "off",
+			},
+		},
+		{
+			files: ["*.test.js"],
+			parser: "espree",
+			parserOptions: {
+				ecmaVersion: 13,
+				sourceType: "module",
+			},
+			extends: ["eslint:recommended", "prettier"],
+			plugins: [],
+			rules: {
+				"no-empty-function": "off",
+			},
+		},
+	],
+	settings: {},
+	env: {
+		es2022: true,
+		node: true,
+	},
+};
