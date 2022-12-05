98d44bca810eaacdd972555ed0923173056f4ddc
Rafał Majewski <goodheropl@gmail.com>
2022-12-06 00:06:33 +0100

    Use HelloModule in the app

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -2,9 +2,10 @@ import {Module} from "@nestjs/common";
 import AppConfigModule from "../app-config/AppConfigModule.js";
 import {AppOrmModule} from "../app-orm/index.js";
 import {CatsModule} from "../features/cats/index.js";
+import HelloModule from "../features/hello/HelloModule.js";
 
 @Module({
-	imports: [CatsModule, AppOrmModule, AppConfigModule],
+	imports: [CatsModule, AppOrmModule, AppConfigModule, HelloModule],
 	controllers: [],
 	providers: [],
 })
