b5ef617704de20fa54ff6416c3bcb6c63dd71d4e
Rafał Majewski <goodheropl@gmail.com>
2023-01-18 17:26:18 +0100

    Explicit type annotations in AppConfig

--- src/app_config/AppConfig.ts
+++ src/app_config/AppConfig.ts
@@ -7,6 +7,7 @@ class AppConfig {
 	// public readonly CUSTOM_HELLO!: string;
 
 	// Optional setting
+	@Type(() => String)
 	@IsString()
 	public readonly CUSTOM_HELLO: string = "Hello customizable world!";
 
@@ -16,6 +17,7 @@ class AppConfig {
 	@Max(65535)
 	public readonly PORT: number = 3000;
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_HOST: string = "localhost";
 
@@ -25,12 +27,15 @@ class AppConfig {
 	@Max(65535)
 	public readonly POSTGRES_PORT: number = 5432;
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_USERNAME: string = "postgres";
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_PASSWORD: string = "postgres";
 
+	@Type(() => String)
 	@IsString()
 	public readonly POSTGRES_DATABASE: string = "postgres";
 }
