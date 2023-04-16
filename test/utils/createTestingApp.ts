7b70ef74aa94c16229b6308c1f0c2f13b46e95cd
Rafał Majewski <goodheropl@gmail.com>
2023-04-16 14:13:44 +0200

    createTestingApp test util function

---
+++ test/utils/createTestingApp.ts
@@ -0,0 +1,15 @@
+import {TestingModule} from "@nestjs/testing";
+import {FastifyAdapter, type NestFastifyApplication} from "@nestjs/platform-fastify";
+import initializeApp from "../../src/initializeApp.js";
+
+export default async function createTestingApp(
+	appTestingModule: TestingModule
+): Promise<NestFastifyApplication> {
+	const testingApp = appTestingModule.createNestApplication<NestFastifyApplication>(
+		new FastifyAdapter()
+	);
+	initializeApp(testingApp);
+	await testingApp.init();
+	await testingApp.getHttpAdapter().getInstance().ready();
+	return testingApp;
+}
