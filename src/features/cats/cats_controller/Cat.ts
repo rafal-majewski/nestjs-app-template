7880ebd492a51098f09acb9745a8fb00e5230328
Rafał Majewski <goodheropl@gmail.com>
2023-04-23 10:41:22 +0200

    Fix applying swagger decorators to a wrong class

--- src/features/cats/cats_controller/Cat.ts
+++ src/features/cats/cats_controller/Cat.ts
@@ -1,6 +1,12 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export default class Cat {
+	@ApiProperty()
 	readonly id!: string;
+	@ApiProperty()
 	readonly name!: string;
+	@ApiProperty()
 	readonly age!: number;
+	@ApiProperty()
 	readonly breed!: string;
 }
